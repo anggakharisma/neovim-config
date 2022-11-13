2875ed7f0e2b0da0be7399d07c42524479800292
Angga Kharisma <anggaputrakharisma@gmail.com>
2022-11-14 01:07:31 +0700

    init

---
+++ lua/general.lua
@@ -0,0 +1,47 @@
+vim.g.mapleader = ','
+
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+
+
+vim.o.updatetime = 300
+vim.incsearch = false
+vim.wo.signcolumn = 'yes'
+
+vim.cmd('set number relativenumber')
+vim.cmd('set nu rnu')
+vim.cmd('hi Pmenu guibg=NONE guifg=NONE')
+vim.cmd('hi PmenuSel guibg=black guifg=yellow')
+vim.cmd('set completeopt=menu,menuone,noselect')
+vim.cmd('set undofile')
+vim.cmd('set undodir=~/.vim/undodir')
+vim.cmd('highlight LineNr term=bold cterm=NONE ctermfg=NONE ctermbg=NONE gui=NONE guifg=NONE guibg=NONE')
+vim.cmd('hi cursorline guibg=#252e47')
+vim.cmd('set cursorline')
+vim.cmd('hi NonText guifg=bg')
+
+
+map('i', 'kk', '<Esc>')
+
+-- Move around splits
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
+
+map('n', '<leader>s', ':w<CR>')
+
+-- NvimTree
+require("nvim-tree").setup()
+map('n', '<C-n>', ':NvimTreeToggle<CR>')            -- open/close
+map('n', '<leader>f', ':NvimTreeRefresh<CR>')       -- refresh
+map('n', '<leader>n', ':NvimTreeFindFile<CR>')      -- search file
---
+++ lua/plugins.lua
@@ -0,0 +1,20 @@
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+
+  use 'tpope/vim-fugitive'
+  use 'airblade/vim-gitgutter'
+
+  use {
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- optional, for file icons
+  },
+  tag = 'nightly' -- optional, updated every week. (see issue #1193)
+}
+end)
