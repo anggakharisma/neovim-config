a66e078daab2163ed86f24c023c83db9dfec7ac1
Angga Kharisma <anggaputrakharisma@gmail.com>
2022-11-14 01:49:19 +0700

    vsnip

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,6 +7,9 @@ require('packer').startup(function(use)
   use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/nvim-cmp'
 
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
+
   use 'tpope/vim-fugitive'
   use 'airblade/vim-gitgutter'
 
